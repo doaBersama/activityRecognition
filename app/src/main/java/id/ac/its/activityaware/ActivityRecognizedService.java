edba7e57323211759f215ea0de9c2dacac1142d8
luthfielaroeha <luthfielaroeha@gmail.com>
2016-10-17 01:34:34 +0700

    ActivityRecognition

---
+++ app/src/main/java/id/ac/its/activityaware/ActivityRecognizedService.java
@@ -0,0 +1,95 @@
+package id.ac.its.activityaware;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import java.util.List;
+
+/**
+ * Created by Zhie on 16/10/2016.
+ */
+
+public class ActivityRecognizedService extends IntentService {
+
+    public ActivityRecognizedService() {
+        super("ActivityRecognizedService");
+    }
+
+    public ActivityRecognizedService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if(ActivityRecognitionResult.hasResult(intent)) {
+            ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+            handleDetectedActivities( result.getProbableActivities() );
+        }
+    }
+
+    private void handleDetectedActivities(List<DetectedActivity> probableActivities) {
+        for( DetectedActivity activity : probableActivities ) {
+            switch( activity.getType() ) {
+                case DetectedActivity.IN_VEHICLE: {
+                    Log.e( "ActivityRecogition", "In Vehicle: " + activity.getConfidence() );
+                    break;
+                }
+                case DetectedActivity.ON_BICYCLE: {
+                    Log.e( "ActivityRecogition", "On Bicycle: " + activity.getConfidence() );
+                    break;
+                }
+                case DetectedActivity.ON_FOOT: {
+                    Log.e( "ActivityRecogition", "On Foot: " + activity.getConfidence() );
+                    break;
+                }
+                case DetectedActivity.RUNNING: {
+                    Log.e( "ActivityRecogition", "Running: " + activity.getConfidence() );
+                    break;
+                }
+                case DetectedActivity.STILL: {
+                    Log.e( "ActivityRecogition", "Still: " + activity.getConfidence() );
+                    if( activity.getConfidence() >= 20 ) {
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+                        builder.setContentText( "Are you still?" );
+                        builder.setSmallIcon( R.mipmap.ic_launcher );
+                        builder.setContentTitle( getString( R.string.app_name ) );
+                        NotificationManagerCompat.from(this).notify(0, builder.build());
+                    }
+                    break;
+                }
+                case DetectedActivity.TILTING: {
+                    Log.e( "ActivityRecogition", "Tilting: " + activity.getConfidence() );
+                    break;
+                }
+                case DetectedActivity.WALKING: {
+                    Log.e( "ActivityRecogition", "Walking: " + activity.getConfidence() );
+                    if( activity.getConfidence() >= 20 ) {
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+                        builder.setContentText( "Are you walking?" );
+                        builder.setSmallIcon( R.mipmap.ic_launcher );
+                        builder.setContentTitle( getString( R.string.app_name ) );
+                        NotificationManagerCompat.from(this).notify(0, builder.build());
+                    }
+                    break;
+                }
+                case DetectedActivity.UNKNOWN: {
+                    Log.e( "ActivityRecogition", "Unknown: " + activity.getConfidence() );
+                    if( activity.getConfidence() >= 20 ) {
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+                        builder.setContentText( "Are you unknown?" );
+                        builder.setSmallIcon( R.mipmap.ic_launcher );
+                        builder.setContentTitle( getString( R.string.app_name ) );
+                        NotificationManagerCompat.from(this).notify(0, builder.build());
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
